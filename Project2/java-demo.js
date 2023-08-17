4b5b8da83f4c1dfee572df6749d8e2f1beb397d7
nicolaGrant1977 <103172849+nicolaGrant1977@users.noreply.github.com>
2023-08-17 15:27:21 +0100

    Add files via upload

---
+++ Project2/java-demo.js
@@ -0,0 +1,18 @@
+
+var string2 = "This is string 2";
+var string3 = "This is string 3";
+
+var newstring = string2 + string3;
+document.write(newstring);
+
+
+function myFunction(){
+	
+	var sentence = "I am learnging a lot ";
+	sentence += "from this book";
+	document.getElementById("btn-demo").innerHTML = sentence;
+	
+}
+
+window.alert("Well hello there");
+
