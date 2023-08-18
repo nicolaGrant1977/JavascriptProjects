651eed9cde2ce559d1a20317a88340cb4aacb819
nicolaGrant1977 <103172849+nicolaGrant1977@users.noreply.github.com>
2023-08-18 08:31:08 +0100

    Add files via upload

--- Project2/java-demo.js
+++ Project2/java-demo.js
@@ -1,18 +1,23 @@
 
-var string2 = "This is string 2";
-var string3 = "This is string 3";
+var string2 = "This is string 2"; // declaring a string	
+var string3 = "This is string 3"; // declaring a string
 
-var newstring = string2 + string3;
-document.write(newstring);
+var newstring = string2 + string3; // concatenating the two strings
+document.write(newstring); //displaying the newly created string value
+string2 += string3; // concatenating string2 to string 3
+document.write(string2); // displaying string2
 
-
-function myFunction(){
+function myFunction(){   // declaring a function
 	
-	var sentence = "I am learnging a lot ";
-	sentence += "from this book";
-	document.getElementById("btn-demo").innerHTML = sentence;
+	var sentence = "I am learnging a lot ";  //declaring a string variable
+	sentence += "from this book"; // concatenating the string with a new value	
+	document.getElementById("btn-demo").innerHTML = sentence;// displaying the new variable to a html value
 	
 }
 
-window.alert("Well hello there");
+window.alert("Well hello there");  // dispalying a string alert
 
+var dad = "Tom", mam = "Mary", kid = "Dave"; // assigning multiply variablesl
+
+document.write(mam);  //Displaying one of the variables
+document.write(3+3); //Dispplaying an expression.
